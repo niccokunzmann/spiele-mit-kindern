3011c70139cd984981a41bf04fbb3b511009ec80
Nicco Kunzmann <niccokunzmann@googlemail.com>
2013-11-24 00:56:02 +0100

    bewegen ausführen und richtung hinzugefügt

--- server.py
+++ server.py
@@ -2,7 +2,10 @@ try:
     import SimpleHTTPServer as http_server
 except ImportError:
     import http.server as http_server
-    
+import socket
+
 print('Url: http://localhost:8000/')
+print('andere IPs: ')
+print(socket.gethostbyname_ex(socket.gethostname()))
 
 http_server.test(http_server.SimpleHTTPRequestHandler)
