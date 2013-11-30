7ab4c97ecb78cd7d9b11c7de1b5f87e74c2ae565
Nicco Kunzmann <niccokunzmann@googlemail.com>
2013-11-30 22:54:19 +0100

    md dateien werden vom server als html ausgeliefert

--- server.py
+++ server.py
@@ -4,8 +4,14 @@ except ImportError:
     import http.server as http_server
 import socket
 
+class MyRequestHandler(http_server.SimpleHTTPRequestHandler):
+    extensions_map = http_server.SimpleHTTPRequestHandler.extensions_map.copy()
+    extensions_map.update({
+        '.md': 'text/html',
+        })
+
 print('Url: http://localhost:8000/')
 print('andere IPs: ')
 print(socket.gethostbyname_ex(socket.gethostname()))
 
-http_server.test(http_server.SimpleHTTPRequestHandler)
+http_server.test(MyRequestHandler)
