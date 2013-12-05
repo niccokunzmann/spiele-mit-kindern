badd01e595941b8ce7b3720c6546465bd75b3297
Nicco Kunzmann <niccokunzmann@googlemail.com>
2013-12-05 13:19:23 +0100

    server wieder vereinfacht.

--- server.py
+++ server.py
@@ -3,19 +3,19 @@ try:
 except ImportError:
     import http.server as http_server
 import socket
-from pprint import pprint
-import urlparse
-import sys
-import os
+##from pprint import pprint
+##import urlparse
+##import sys
+##import os
 
-try:
-    __file__
-except NameError:
-    __file__ = os.path.join(os.getcwd(), 'server.py')
-baseDir = os.path.dirname(__file__)
-sys.path.insert(0, os.path.join(baseDir, 'python'))
-
-import githubInterface
+##try:
+##    __file__
+##except NameError:
+##    __file__ = os.path.join(os.getcwd(), 'server.py')
+##baseDir = os.path.dirname(__file__)
+##sys.path.insert(0, os.path.join(baseDir, 'python'))
+##
+##import githubInterface
 
 class MyRequestHandler(http_server.SimpleHTTPRequestHandler):
     extensions_map = http_server.SimpleHTTPRequestHandler.extensions_map.copy()
@@ -23,51 +23,57 @@ class MyRequestHandler(http_server.SimpleHTTPRequestHandler):
         '.md': 'text/plain',
         })
 
-    def do_POST(self):
-        pprint(dict(self.headers))
-        length = self.headers.getheader('content-length')
-        if length:
-            content = self.rfile.read(int(length))
-            # print 'content:', content
-            params = urlparse.parse_qs(content) # urllib.urlencode
-            pprint(params)
-            comment = params.get('comment', [''])[0]
-            sourceCode = params.get('sourceCode', [''])[0]
-        return self.do_GET()
+print('Url: http://localhost:8000/')
+print('andere IPs: ')
+print(socket.gethostbyname_ex(socket.gethostname()))
+
+http_server.test(MyRequestHandler)
 
 
-def test(HandlerClass = BaseHTTPRequestHandler,
-         ServerClass = HTTPServer, protocol="HTTP/1.0", port=8000):
-    """Test the HTTP request handler class.
 
-    This runs an HTTP server on port 8000 (or the first command line
-    argument).
+##    def do_POST(self):
+##        pprint(dict(self.headers))
+##        length = self.headers.getheader('content-length')
+##        if length:
+##            content = self.rfile.read(int(length))
+##            # print 'content:', content
+##            params = urlparse.parse_qs(content) # urllib.urlencode
+##            pprint(params)
+##            comment = params.get('comment', [''])[0]
+##            sourceCode = params.get('sourceCode', [''])[0]
+##        return self.do_GET()
+##
 
-    """
-    server_address = ('', port)
-
-    HandlerClass.protocol_version = protocol
-    httpd = ServerClass(server_address, HandlerClass)
-
-    sa = httpd.socket.getsockname()
-    print("Serving HTTP on", sa[0], "port", sa[1], "...")
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        print("\nKeyboard interrupt received, exiting.")
-        httpd.server_close()
-        sys.exit(0)
-
-def start_locally():
-    print('Url: http://localhost:8000/')
-    print('andere IPs: ')
-    print(socket.gethostbyname_ex(socket.gethostname()))
-
-    test(MyRequestHandler)
-
-def start_internet():
-    test(MyRequestHandler, port = 80)
-
-if __name__ == '__main__':
-    start_locally()
+##def test(HandlerClass = BaseHTTPRequestHandler,
+##         ServerClass = HTTPServer, protocol="HTTP/1.0", port=8000):
+##    """Test the HTTP request handler class.
+##
+##    This runs an HTTP server on port 8000 (or the first command line
+##    argument).
+##
+##    """
+##    server_address = ('', port)
+##
+##    HandlerClass.protocol_version = protocol
+##    httpd = ServerClass(server_address, HandlerClass)
+##
+##    sa = httpd.socket.getsockname()
+##    print("Serving HTTP on", sa[0], "port", sa[1], "...")
+##    try:
+##        httpd.serve_forever()
+##    except KeyboardInterrupt:
+##        print("\nKeyboard interrupt received, exiting.")
+##        httpd.server_close()
+##        sys.exit(0)
+##
+##def start_locally():
+##
+##    test(MyRequestHandler)
+##
+##def start_internet():
+##    test(MyRequestHandler, port = 80)
+##
+##if __name__ == '__main__':
+##    start_locally()
+##
 
